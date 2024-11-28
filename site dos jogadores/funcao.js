7861190cd16d10b5afc9893a4a8c4fc364a66004
Isaias Araujo <araujoisaias58@gmail.com>
2024-11-28 19:43:35 -0300

    Add files via upload

---
+++ site dos jogadores/funcao.js
@@ -0,0 +1,12 @@
+//dom
+const osvaldo = document.querySelector('osvaldo1')
+const lucas = document.querySelector('arcanjo1')
+const wagner = document.querySelector('wagner1')
+
+//evento
+osvaldo.addEventListener('click', mostrar1)
+
+//função
+function mostrar1(){
+    
+}
